365c4256a5e36cb99b7b1c4d31f46788b39ac68d
ifkimb <kcarolinw0704@gmail.com>
2025-08-22 21:20:48 -0300

    refactor

--- app/src/main/java/br/com/etecia/myapp/AcessoriosFragment.java
+++ app/src/main/java/br/com/etecia/myapp/AcessoriosFragment.java
@@ -13,7 +13,8 @@ public class AcessoriosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_acessorios, container, false);
+        View view =  inflater.inflate(R.layout.fragment_acessorios, container, false);
+
+        return view;
     }
 }
--- app/src/main/java/br/com/etecia/myapp/LojaFragment.java
+++ app/src/main/java/br/com/etecia/myapp/LojaFragment.java
@@ -13,7 +13,9 @@ public class LojaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_loja, container, false);
+        View view =  inflater.inflate(R.layout.fragment_loja, container, false);
+
+
+        return view;
     }
 }
