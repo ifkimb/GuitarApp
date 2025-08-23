6e8153ea39d4ee378594b32a50c80386de32c22d
ifkimb <kcarolinw0704@gmail.com>
2025-08-22 21:55:51 -0300

    refactor

--- app/src/main/java/br/com/etecia/myapp/AdapterItensGuitarra.java
+++ app/src/main/java/br/com/etecia/myapp/AdapterItensGuitarra.java
@@ -1,4 +1,23 @@
 package br.com.etecia.myapp;
 
-public class AdapterItensGuitarra {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class AdapterItensGuitarra extends FragmentStateAdapter {
+    public AdapterItensGuitarra(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
