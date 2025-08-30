df618c663f1bb4e189d26541b6910efaaa276782
ifkimb <kcarolinw0704@gmail.com>
2025-08-29 22:19:18 -0300

    commit

--- app/src/main/java/br/com/etecia/myapp/GuitarraFragment.java
+++ app/src/main/java/br/com/etecia/myapp/GuitarraFragment.java
@@ -3,6 +3,7 @@ package br.com.etecia.myapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,18 +14,15 @@ import java.util.List;
 
 public class GuitarraFragment extends Fragment {
 
+    RecyclerView idRecyclerViewGuitarra;
+
     List<Guitarra> lstGuitarra;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_guitarra, container, false);
-
-
-        return view;
-    }
-
-    lstGuitarra = new ArrayList<>();
+        lstGuitarra = new ArrayList<>();
         lstGuitarra.add(new Guitarra(R.drawable.guitarra_allblack, "guitarra all black"));
         lstGuitarra.add(new Guitarra(R.drawable.guitarra_azul, "guitarra azul"));
         lstGuitarra.add(new Guitarra(R.drawable.guitarra_branca, "guitarra branca"));
@@ -32,14 +30,13 @@ public class GuitarraFragment extends Fragment {
         lstGuitarra.add(new Guitarra(R.drawable.guitarra_strinberg, "guitarra strinberg"));
         lstGuitarra.add(new Guitarra(R.drawable.guitarra_vermelha, "guitarra vermelha"));
 
-    idRecyclerViewGuitarra = findViewById(R.id.idRecyclerViewGuitarra);
+        idRecyclerViewGuitarra = view.findViewById(R.id.idRecyclerViewGuitarra);
 
-    AdaptadorGuitarra adapter = new AdaptadorGuitarra(getApplicationContext(),lstGuitarra);
+        AdaptadorGuitarra adapter = new AdaptadorGuitarra(getContext(),lstGuitarra);
 
-    idToolBarItens = findViewById(R.id.idToolBarItens);
+        return view;
+    }
 
-    idViewPagerItensGuitarra = findViewById(R.id.idViewPagerItensGuitarra);
 
-    idTabLayoutItensGuitar = findViewById(R.id.idTabLayoutItensGuitar);
 
 }
