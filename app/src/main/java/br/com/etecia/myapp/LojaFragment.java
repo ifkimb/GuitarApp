c214b3408c9977b134335852d0cc295a437ee885
ifkimb <kcarolinw0704@gmail.com>
2025-09-26 22:15:03 -0300

    adaptador loja

--- app/src/main/java/br/com/etecia/myapp/LojaFragment.java
+++ app/src/main/java/br/com/etecia/myapp/LojaFragment.java
@@ -3,18 +3,30 @@ package br.com.etecia.myapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LojaFragment extends Fragment {
+
+    RecyclerView idRecyclerViewLoja;
+
+    List<Loja> lstLoja;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_loja, container, false);
 
+        idRecyclerViewLoja = view.findViewById(R.id.idRecyclerViewLoja);
+
+        lstLoja = new ArrayList<>();
+        lstLoja.add(new Loja());
 
         return view;
     }
